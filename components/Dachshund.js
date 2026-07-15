23cab25ec8861844b63eb3f9a82adcc3ef31f3f9
FilipSeoCC <fkedziorawenet@gmail.com>
2026-07-15 21:37:21 +0200

    Zastap CSS-owego jamnika prawdziwa ilustracja (public/dachshund.png)
    Obrazek dostarczony przez uzytkownika, tlo usuniete/przycieta (Pillow), podpiety
    przez next/image w komponencie Dachshund. Usunieto nieuzywane juz style
    CSS-owego rysunku (body/head/eye/ear/tail/legs/bow) i keyframes wag/eye-sparkle,
    zachowano animacje mood/escape (dog-breathe, happy-hop, run-away) oraz zart
    z "ukradziona koscia" NIE.

--- components/Dachshund.js
+++ components/Dachshund.js
@@ -1,34 +1,17 @@
+import Image from "next/image";
+
 export default function Dachshund({ mood = "sweet", escaping = false }) {
   return (
     <div className={`dog-wrap ${mood === "happy" ? "dog-happy" : ""} ${escaping ? "dog-escape" : ""}`} aria-hidden="true">
       <div className="dog">
-        <div className="tail" />
-        <div className="body">
-          <span className="spot spot-one" />
-          <span className="spot spot-two" />
-          <span className="body-shine" />
-        </div>
-        <div className="head">
-          <div className="ear" />
-          <div className="eyebrow" />
-          <div className="eye">
-            <span className="eye-shine" />
-            <span className="eye-shine eye-shine-small" />
-          </div>
-          <div className="eyelash" />
-          <div className="snout">
-            <span className="snout-shine" />
-          </div>
-          <div className="nose">
-            <span className="nose-shine" />
-          </div>
-          <div className="cheek" />
-        </div>
-        <div className="leg leg-one" />
-        <div className="leg leg-two" />
-        <div className="leg leg-three" />
-        <div className="leg leg-four" />
-        <div className="bow">{"♥"}</div>
+        <Image
+          className="dog-image"
+          src="/dachshund.png"
+          alt=""
+          width={442}
+          height={242}
+          priority
+        />
         {escaping ? <div className="stolen-bone">NIE</div> : null}
       </div>
     </div>
